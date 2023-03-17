41d1f5ff9c25c4ed4a454f248c5a18e13dc827a5
fbar99 <francisco@ant-automation.com>
2023-03-16 21:58:22 -0500

    implemented edit service functionality

--- frontend/src/components/serviceDetails.vue
+++ frontend/src/components/serviceDetails.vue
@@ -4,47 +4,49 @@ import { required } from '@vuelidate/validators'
 import axios from 'axios'
 import { DateTime } from 'luxon'
 import { useServiceListStore } from "@/store/serviceStore";
+import { useLoggedInUserStore } from "@/store/loggedInUser";
 const apiURL = import.meta.env.VITE_ROOT_API
 
 export default {
   props: ['id'],
   setup() {
-    return { v$: useVuelidate({ $autoDirty: true }) }
+    const list = useServiceListStore();
+    const user = useLoggedInUserStore();
+    return { v$: useVuelidate({ $autoDirty: true }),
+  list,
+  user }
   },
   data() {
-    const list = useServiceListStore();
     return {
-      list: [],
         services: {
           name: '',
           status: '',
-          description: '',
-          services: []
+          description: ''
         }
       }
     },
   
   created() {
-    axios.get(`${apiURL}/services/id/${this.$route.params.id}`).then((res) => {
-      this.list.services = res.data
-    })
+      const index = this.list.serviceList.findIndex((dict) => dict.id == this.$route.params.id);
+      this.services.name = this.list.serviceList[index].name
+      this.services.status = this.list.serviceList[index].status
+      this.services.description = this.list.serviceList[index].description
+      console.log(this.services.name)
   },
 
   methods: {
     handleServiceUpdate() {
-      axios.put(`${apiURL}/services/update/${this.id}`, this.services).then(() => {
-        alert('Update has been saved.')
+        this.list.edit(this.$route.params.id, this.services.name, this.services.status, this.services.description);
         this.$router.back()
-      })
     },
     editService(servicesID) {
       this.$router.push({ name: 'servicedetails', params: { id: servicesID } })
     },
     serviceDelete() {
-      axios.delete(`${apiURL}/services/${this.id}`).then(() => {
-        alert('Service has been deleted.')
-        this.$router.push({ name: 'findservices' })
-      })
+        console.log(this.$route.params.id);
+        console.log(typeof this.$route.params.id)
+        this.list.delete(parseInt(this.$route.params.id));
+        //this.$router.push({ name: 'findservices' })
     }
   },
   // sets validations for the various data properties
@@ -136,68 +138,13 @@ export default {
           <div></div>
           <div></div>
           <!-- form field -->
-          <div class="flex flex-col grid-cols-3">
-            <label>Services Offered at Event</label>
-            <div>
-              <label for="familySupport" class="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="familySupport"
-                  value="Family Support"
-                  v-model="services.services"
-                  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
-                  notchecked
-                />
-                <span class="ml-2">Family Support</span>
-              </label>
-            </div>
-            <div>
-              <label for="adultEducation" class="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="adultEducation"
-                  value="Adult Education"
-                  v-model="services.services"
-                  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
-                  notchecked
-                />
-                <span class="ml-2">Adult Education</span>
-              </label>
-            </div>
-            <div>
-              <label for="youthServices" class="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="youthServices"
-                  value="Youth Services Program"
-                  v-model="services.services"
-                  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
-                  notchecked
-                />
-                <span class="ml-2">Youth Services Program</span>
-              </label>
-            </div>
-            <div>
-              <label for="childhoodEducation" class="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  id="childhoodEducation"
-                  value="Early Childhood Education"
-                  v-model="services.services"
-                  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
-                  notchecked
-                />
-                <span class="ml-2">Early Childhood Education</span>
-              </label>
-            </div>
-          </div>
         </div>
 
         <!-- grid container -->
         <div
           class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
         >
-          <div class="flex justify-between mt-10 mr-20">
+          <div v-if="user.isLoggedIn && user.role == 'editor'" class="flex justify-between mt-10 mr-20">
             <button
               @click="handleServiceUpdate"
               type="submit"
@@ -206,7 +153,7 @@ export default {
               Update Service
             </button>
           </div>
-          <div class="flex justify-between mt-10 mr-20">
+          <div v-if="user.isLoggedIn && user.role == 'editor'" class="flex justify-between mt-10 mr-20">
             <button
               @click="serviceDelete"
               type="submit"
